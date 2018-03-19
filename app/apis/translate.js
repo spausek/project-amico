0f5ecca6ef221726b719e0c524a29c292d9e88eb
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-03-18 18:32:23 -0700

    merging translate and routes from phil before push to master

---
+++ app/apis/translate.js
@@ -0,0 +1,29 @@
+const translate = require("google-translate-api");
+
+
+
+const amicosTranslate = function (message, languageOne, languageTwo) {
+
+translate(message, { from: languageOne, to: languageTwo })
+  .then(res => {
+    console.log(res.from.text.value);
+    console.log(res.from.text.autoCorrected);
+    console.log(res.text);
+    console.log(res.from.text.didYouMean);
+    const translated = {
+        originalMessage: message,
+        autoCorrectedMessage: res.from.text.value,
+        correctedMessage: res.from.text.autoCorrected,
+        dymMessage: res.from.text.didYouMean,
+        translatedMessage: res.text,
+    }
+    console.log("The console says: ", translated);
+    return translated;
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+amicosTranslate("whut do you want to do today?", 'en', 'es');
+
+module.exports = amicosTranslate;
--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -2,24 +2,27 @@ const Express = require('express');
 const Path = require('path');
 const Router = Express.Router();
 
+
     
- Router.get("/", function(req, res) {
-    res.sendFile(Path.join(__dirname, "/../public/index.html"));
-  });
+Router.get("/", function(req, res) {
+    res.sendFile(Path.join(__dirname, "../public/index.html"));
+});
   
- Router.get("/login", function(req,res){
- 	res.sendFile(Path.join(__dirname + "/../public/login.html"))
- })
+Router.get("/login",function(req,res){
+	res.sendFile(Path.join(__dirname, "../public/login.html"));
+})
 
- Router.get("/profile", function(req,res){
- 	
- 	res.sendFile(Path.join(__dirname + "/../public/profile.html"))
- })
+Router.get("/home", function(req, res) {
+    res.sendFile(Path.join(__dirname, "../public/home.html"));
+});
 
- 
+Router.get("/profile", function(req, res) {
+    res.sendFile(Path.join(__dirname, "../public/profile.html"));
+});
 
-
-
+Router.get("/tutoring", function(req, res) {
+  res.sendFile(Path.join(__dirname, "../public/tutoring.html"));
+});
 
 
 module.exports = Router;
