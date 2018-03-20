41aeca247f67cbeef5530cd0dccadd9c06653ec0
haigbedros <haigbedros@gmail.com>
2018-03-20 12:16:05 -0700

    home page html css js update expand input fix message modal added

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -8,7 +8,19 @@ $(document).ready(function() {
       
       $('#count_message').html(text_remaining + ' remaining');
     });
+
+    $('input.expand').blur(function () {
+        $(this).animate({ width: "9.7em"}, 500); 
+    });
+
+    $('input.expand').focus(function () {
+        $(this).animate({ width: "15em"}, 500); 
+    });
     
+    $('textarea.expand').blur(function () {
+        $(this).animate({ height: "2.4em" }, 500); 
+    });
+
     $('textarea.expand').focus(function () {
         $(this).animate({ height: "8em" }, 500); 
     });
