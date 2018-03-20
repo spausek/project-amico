a3d2fcca2f7e269ece5298a8d57e278ef1f3cc67
phild6596 <phild6596@gmail.com>
2018-03-19 20:55:30 -0700

    made language.js which holds an object of language codes.

---
+++ app/public/assets/js/language.js
@@ -0,0 +1,57 @@
+
+  const languages = 
+    {
+        "Afrikaans": "af",
+        "Albanian": "sq",
+        "Arabic": "ar",
+        "Armenian": "hy",
+        "Belarusian": "be",
+        "Catalan": "ca",
+        "Chinese-simple": "zh-CN",
+        "Croatian": "hr",
+        "Czech": "cs",
+        "Danish": "da",
+        "English": "en",
+        "Estonian": "et",
+        "Filipino": "tl",
+        "Finnish": "fi",
+        "French": "fr",
+        "German": "de",
+        "Greek": "el",
+        "Hebrew": "iw",
+        "Hindi": "hi",
+        "Hungarian": "hu",
+        "Indonesian": "id",
+        "Irish": "ga",
+        "Italian": "it",
+        "Japanese": "ja",
+        "Korean": "ko",
+        "Norwegian": "no",
+        "Persian": "fa",
+        "Polish": "pl",
+        "Portuguese": "pt",
+        "Russian": "ru",
+        "Spanish": "es",
+        "Swedish": "sv",
+        "Thai": "th",
+        "Turkish": "tr",
+        "Ukrainian": "uk",
+        "Vietnamese": "vi",
+        "Yiddish":"yi"
+    };
+  
+  
+
+  for (language in languages) {
+      console.log(languages[language]);
+  }
+
+
+    // const languageOne = languages.map(spoken => {
+    //     return spoken;
+    // });
+    //console.log(languageOne);
+// console.log(languages.slice(0));
+
+
+
