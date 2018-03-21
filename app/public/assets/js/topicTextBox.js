fc42275a81cd2df12ae2f1ea80823cc1f2d2c8a7
haigbedros <haigbedros@gmail.com>
2018-03-20 19:40:22 -0700

    adding comment bar under posts

--- app/public/assets/js/topicTextBox.js
+++ app/public/assets/js/topicTextBox.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     var text_max = 200;
     $('#count_message').html(text_max + ' remaining');
     
-    $('#text').keyup(function() {
-      var text_length = $('#text').val().length;
+    $('#messageInput').keyup(function() {
+      var text_length = $('#messageInput').val().length;
       var text_remaining = text_max - text_length;
       
       $('#count_message').html(text_remaining + ' remaining');
