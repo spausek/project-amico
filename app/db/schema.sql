725fef954e84d164b6304030f01d8379ebe42b71
phild6596 <phild6596@gmail.com>
2018-03-22 20:39:08 -0700

    deleted the old MySql database and created a new one to hold the languages and the language codes

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -2,10 +2,9 @@ DROP DATABASE IF EXISTS language_learners;
 CREATE DATABASE language_learners;
 USE language_learners;
 
-CREATE TABLE amico_users  
+CREATE TABLE amico_languages  
 (
     id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    amico_user_name VARCHAR(300) NOT NULL,
     native_language VARCHAR(200) NOT NULL,
-    learning_language VARCHAR(200) NOT NULL
+    language_code VARCHAR(200) NOT NULL
 );
--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -1,2 +1,72 @@
-INSERT INTO amico_users (amico_user_name, native_language, learning_language)
-VALUES('Phil Dominguez', 'English', 'Hebrew');
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Afrikans", "af");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Albanian", "sq");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Armenian", "hy");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Belarusian", "be");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Catalan", "ca");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Chinese-simple", "zh-CN");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Croatian", "hr");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Czech", "cs");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Danish", "da");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("English", "en");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Estonian", "et");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Filipino", "tl");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Finnish", "fi");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("French", "fr");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("German", "de");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Greek", "el");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Hebrew", "iw");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Hindi", "hi");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Hungarian", "hu");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Indonesian", "id");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Irish", "ga");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Italian", "it");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Japanese", "ja");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Korean", "ko");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Norwegian", "no");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Persian", "fa");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Polish", "pl");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Portuguese", "pt");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Russian", "ru");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Spanish", "es");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Swedish", "sv");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Thai", "th");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Turkish", "tr");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Ukranian", "uk");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Vietnamese", "vi");
+INSERT INTO amico_languages (native_language, language_code)
+VALUES("Yiddish", "yi");
