44c39e9566ff0cb0346c307e71687ea6bfc7d048
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-03-22 19:41:56 -0700

    Resolved conflict

--- app/public/assets/js/login.js
+++ app/public/assets/js/login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $( document ).ready(function(){
 
   const ui = new firebaseui.auth.AuthUI(firebase.auth());
@@ -17,16 +16,6 @@ $(".register form").on("submit", function(event){
     event.preventDefault();
         $(document).ready(function() {
 
-=======
-$(document).ready(function() {
-
-  firebase.initializeApp(config);
-
-  $(".register modal-body form").on("submit", function(event){
-    event.preventDefault();
-        $(document).ready(function() {
-  
->>>>>>> e269ade37ef454fcdad956f9420d243283ffea58
     var email = $(".register .email").val();
     var password = $(".register .password").val();
     
@@ -37,7 +26,6 @@ $(document).ready(function() {
         .catch(function(err){
             console.log(err);
         });   
-<<<<<<< HEAD
       });
 
     })
@@ -56,41 +44,3 @@ $(document).ready(function() {
 
 
 
-=======
-  });
-  
-  });
-    
-  });
-  
-// require('dotenv').config();
-// dotenv.config();
-
-// Initialize Firebase
-// var config = {
-//   apiKey: "AIzaSyALJlqbV2CBSFy-nTDjDn5N_0fow6CiziU",
-//   authDomain: "user-auth-e01be.firebaseapp.com",
-//   databaseURL: "https://user-auth-e01be.firebaseio.com",
-//   projectId: "user-auth-e01be",
-//   storageBucket: "",
-//   messagingSenderId: "387653187343"
-// };
-// firebase.initializeApp(config);
-
-// // FirebaseUI config.
-// var uiConfig = {
-//   signInSuccessUrl: 'https://media.giphy.com/media/rmi45iyhIPuRG/giphy.gif',
-//   signInOptions: [
-//     // Leave the lines as is for the providers you want to offer your users.
-//     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//   ],
-//   // Terms of service url.
-//   tosUrl: '<your-tos-url>'
-// };
-
-// // Initialize the FirebaseUI Widget using Firebase.
-// var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// // The start method will wait until the DOM is loaded.
-// ui.start('#firebaseui-auth-container', uiConfig);
->>>>>>> e269ade37ef454fcdad956f9420d243283ffea58
